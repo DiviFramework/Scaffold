19a3986e8254e0e01bcc747d222e0df331b07fc0
Ketan Shah <ketan.shah@gmail.com>
2017-11-24 12:24:40 +0530

    updated phpunit bootstrap

--- templates/plugin/tests/bootstrap.php
+++ templates/plugin/tests/bootstrap.php
@@ -5,10 +5,7 @@
  * @package {{package_name}}
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
+$_tests_dir = 'wordpress-tests-lib';
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
@@ -16,10 +13,11 @@ require_once $_tests_dir . '/includes/functions.php';
 /**
  * Manually load the plugin being tested.
  */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/{{plugin_slug}}.php';
+function _manually_load_plugin()
+{
+    require dirname(dirname(__FILE__)) . '/{{plugin_slug}}.php';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
